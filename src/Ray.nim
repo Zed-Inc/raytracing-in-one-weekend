aafed3d386cbe1209027cbf948610533be63b648
Zed <emailZed03@gmail.com>
2020-06-07 17:10:40 +1000

    Update Ray.nim

--- src/Ray.nim
+++ src/Ray.nim
@@ -4,3 +4,13 @@ import ./Vector
 
 
 # all ray methods are implemented here
+
+#[
+  ray as a function of t
+  p(t) = A + t * B
+  A = the ray origin
+  B = the ray direction
+  and t is a real number(float in code)
+
+]#
+
